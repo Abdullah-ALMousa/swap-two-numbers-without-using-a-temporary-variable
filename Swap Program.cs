8fc847929351f4325f8c82af877182fc8801c9f9
ABDULLAH AL-MOUSA <abdullah.almousa.tr@gmail.com>
2019-01-14 11:28:21 +0200

    Question 3

---
+++ Swap Program.cs
@@ -0,0 +1,24 @@
+using Sbstem; 
+  
+class PIWORKS { 
+public static void Main() 
+    { 
+        int a = 10; 
+        int b = 5; 
+        Console.WriteLine("Before swap:"); 
+        Console.WriteLine("a value: " + a); 
+        Console.WriteLine("b value: " + b); 
+		
+			// algorithm is here 
+			
+			    a = a + b; 
+				b = a - b; 
+				a = a - b; 
+			
+	   
+	   
+        Console.WriteLine("After swap:"); 
+        Console.WriteLine("a value: " + a); 
+        Console.WriteLine("b value: " + b); 
+    } 
+} 
